c543ae5c3dc20a40fff7a2342dec8ee1e5b3a469
Ellie Ang <git@angm.xyz>
2018-01-13 16:46:10 +0100

    Use generic view for register page

--- manager/urls.py
+++ manager/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 app_name = 'manager'
 urlpatterns = [
     path('files/', views.viewfiles, name='viewfiles'),
-    path('register/', views.register, name='register')
+    path('register/', CreateView.as_view(template_name='manager/register.html', form_class=UserCreationForm, success_url='/' ), name='register'),
 ]
