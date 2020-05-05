75a710f381b8516a17778f6f0c262f94852f7427
Ellie Ang <git@angm.xyz>
2020-05-05 16:40:44 +0200

    Prevent 500 when file does not exist

--- downloader/views.py
+++ downloader/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from zerofile.models import File
 from zerofile.settings import FILEDIR
 
 def download(request, file_id):
     """ Serve file corresponding to the id """
     dbfile = get_object_or_404(File, _id=file_id)
-    with open(FILEDIR + dbfile._id, "rb") as i:
-        response = HttpResponse(i.read(), content_type=dbfile.mime)
-        response['Content-Disposition'] = 'inline; filename=%s' % dbfile.name
-        return response
+    try:
+        with open(FILEDIR + dbfile._id, "rb") as i:
+            response = HttpResponse(i.read(), content_type=dbfile.mime)
+            response['Content-Disposition'] = 'inline; filename=%s' % dbfile.name
+            return response
+    except:
+        raise Http404("File not found")
