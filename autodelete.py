e97eced17dfcadd04b28325bce3f66ecd25f2876
Ellie Ang <git@angm.xyz>
2020-12-31 22:13:22 +0100

    Slight improvements to autodelete

--- autodelete.py
+++ autodelete.py
@@ -3,7 +3,7 @@ from django.core.files.storage import default_storage
 from django.utils import timezone
 from django_extensions.management.jobs import BaseJob
 from zerofile.models import File
-from zerofile.settings import FILEDIR
+from zerofile.settings import DEBUG, FILEDIR
 
 def timer():
     next_call = time.time()
@@ -13,17 +13,16 @@ def timer():
         time.sleep(next_call - time.time())
 
 def execute():
-    print("deleting expired files...")
-    files = File.objects.exclude(expiry__gte=timezone.now())
+    files = File.objects.filter(deleted=False).exclude(expiry__gte=timezone.now())
     for file in files:
         delete_file(file._id)
         file.deleted = True
         file.save()
-        print("deleted: " + file.name)
-    print("delete job finished")
 
 def delete_file(fileid):
     default_storage.delete(FILEDIR + fileid)
 
-timerThread = threading.Thread(target=timer)
-timerThread.start()
+if not DEBUG:
+    print("Running without DEBUG, enabling file autodelete...")
+    timerThread = threading.Thread(target=timer)
+    timerThread.start()
